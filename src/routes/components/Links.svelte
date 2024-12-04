79be0feb93660d43718fe90b147aa404834cb2a8
allsers <162462688+allsers@users.noreply.github.com>
2024-12-04 19:25:05 +0100

    Update Links.svelte

--- src/routes/components/Links.svelte
+++ src/routes/components/Links.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" media="print">
 </svelte:head>
 
 
