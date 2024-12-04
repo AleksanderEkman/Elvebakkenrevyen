cbb0d4eb76665e1b599efd1195029d21ed5fcd0d
allsers <162462688+allsers@users.noreply.github.com>
2024-12-04 12:17:54 +0100

    not loading laxy

--- src/routes/components/Slideshow.svelte
+++ src/routes/components/Slideshow.svelte
@@ -35,7 +35,6 @@
   <div class="slideshow" aria-live="polite" aria-atomic="true">
     {#key imageIndex}
       <img 
-        loading="lazy"
         id="slideshow-image" 
         src={imageArray[imageIndex]} 
         alt={`Elvebakkenrevybilde ${imageIndex + 1} av ${imageArray.length}`} 
