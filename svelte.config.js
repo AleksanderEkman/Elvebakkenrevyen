60d3e72404535fc81bddc82c517c76fd67c58b35
allsers <162462688+allsers@users.noreply.github.com>
2024-12-07 23:02:26 +0100

    csp fix

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,29 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
+		csp: {
+			mode: "hash",
+			directives: {
+				"script-src": [
+					"self",
+					"https://www.googletagmanager.com",
+					'sha256-1xOgbz/xSP7IfjQKKYNLJO6q+HLQu66Wjd4osOvrWR8='
+				],
+			},
+		},
+		files: {
+			hooks: {
+				server: 'src/hooks/server.js',
+				universal: 'src/hooks/universal.js'
+			},
+		},
+		prerender: {
+			entries: ['*']
+		},
 	}
 };
 
