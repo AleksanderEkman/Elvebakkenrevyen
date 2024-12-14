e5a8921590c0b720bda485c25f733e4e6affc4b2
allsers <162462688+allsers@users.noreply.github.com>
2024-12-14 17:38:46 +0100

    smooth scrolling test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,27 @@
     import { onMount } from 'svelte';
 	let { children } = $props();
     import Footer from './Footer.svelte';
-
-
+    import { ScrollWrapper } from '@shentohendriks/svelte-smoothscroll';
+    let lenisOptions: {
+        wrapper: window,
+        content: document.documentElement,
+        lerp: 0.1,
+        duration: 1.2,
+        orientation: 'vertical',
+        gestureOrientation: 'vertical',
+        smoothWheel: true,
+        smoothTouch: true,
+        wheelMultiplier: 1,
+        touchMultiplier: 2,
+        normalizeWheel: true
+    }
 </script>
 
-{@render children()}
-<Footer />    
 
+<ScrollWrapper settings={lenisOptions}>
+    {@render children()}
+    <Footer />    
+</ScrollWrapper>
 <style>
-    /* */
+    /*  */
 </style>
