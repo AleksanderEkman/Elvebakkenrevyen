642f55c572f931f08583e32ed07e0f87e09c4a4e
allsers <162462688+allsers@users.noreply.github.com>
2024-12-20 13:58:51 +0100

    Update Footer.svelte

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -42,7 +42,7 @@
     align-items: center;
     width: 100vw;
     padding: 2.75em 0em;
-    background: linear-gradient(135deg, #000000, #161616);
+    background: linear-gradient(135deg, #000000, #101010);
     box-shadow: 0 -3px 10px rgba(0, 0, 0, 0.5);
     z-index: 1;
   }
